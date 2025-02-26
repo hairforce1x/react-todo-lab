e78cc848d098441346d6e97087a4d0f9bd8e89b5
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-02-25 20:04:23 -0700

    Added data.jsx and farted around with App.jsx

--- src/modules/List.jsx
+++ src/modules/List.jsx
@@ -1,9 +1,12 @@
 // import { initialState } from '../assets/data'
 
-export default function List(){
+export default function List() {
     return (
         <>
-            <h3>List</h3>
+            <div>
+                <input />
+                <button>add task</button>
+            </div>
         </>
     )
 }
