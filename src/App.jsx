f892294737c2b04fc03eb14f72e251c9ec06ecbd
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-02-25 19:41:54 -0700

    For Oussama

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Heading from './modules/Heading'
+import List from './modules/List'
+function App() {
+
+  return (
+    <>
+      <Heading />
+      <List />
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/List.jsx
@@ -0,0 +1,9 @@
+// import { initialState } from '../assets/data'
+
+export default function List(){
+    return (
+        <>
+            <h3>List</h3>
+        </>
+    )
+}
