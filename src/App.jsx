e9e5653d77394f8e3192585efdd1b05e59bc9084
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-02-26 11:44:31 -0700

    Added handleEdit and handleDelete dispatch functions.

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,26 @@ function reducer(tasks, action){
     case 'add': {
       return console.log('added')
     }
+    case 'delete': {
+      console.log('deleted')
+    }
+    case 'edit': {
+      console.log('edited')
+    }
   }
       throw Error('danger will robinson')   
 }
 
 export default function AddTasks() {
   const [tasks, dispatch] = useReducer(reducer, initialState);
-
   function handleAdd(text) {
     dispatch({type: 'add', text: text})
+  }
+  function handleDelete(text) {
+    dispatch({type: 'delete', text: ''})
+  }
+  function handleEdit(text) {
+    dispatch({type: 'edit', text: text})
   }
 
   return (
@@ -26,6 +37,8 @@ export default function AddTasks() {
       <List
         tasks={tasks}
         handleAdd={handleAdd}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
       />
     </>
   )
--- src/modules/List.jsx
+++ src/modules/List.jsx
@@ -1,27 +1,31 @@
 import initialState from '../data'
 import { useState } from 'react'
 
-export default function List({ handleAdd }) {
+export default function List({ handleAdd, handleDelete, handleEdit }) {
 
     const [text, setTask] = useState('');
 
     return (
-            <div>
-                <input value={text} onChange={e => setTask(e.target.value)} />
-                <button onClick={() => {
-                    console.log(text)
-                    handleAdd(text)
-                }}>add task</button>
-                <ul>
-                    {initialState.map(test => (
-                        <li>
-                            <label><input type='checkbox' /></label>
-                            {test.title}
-                            <button>Delete</button>
-                            <button>Edit</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div>
+            <input value={text} onChange={e => setTask(e.target.value)} />
+            <button onClick={() => {
+                handleAdd(text)
+                setTask('')
+            }}>add task</button>
+            <ul>
+                {initialState.map(test => (
+                    <li>
+                        <label><input type='checkbox' /></label>
+                        {test.title}
+                        <button onClick={() => {
+                            handleDelete(text)
+                        }}>Delete</button>
+                        <button onClick={() => {
+                            handleEdit(text)
+                        }}>Edit</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
