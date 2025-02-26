788093ab34097eae1f1a7f031b4431f666b393ef
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-02-25 20:37:40 -0700

    UI complete. No functionality

--- src/modules/List.jsx
+++ src/modules/List.jsx
@@ -3,21 +3,19 @@ import initialState from '../data'
 export default function List() {
     console.log(initialState)
     return (
-        <>
             <div>
                 <input />
                 <button>add task</button>
-            </div>
-            <div>
                 <ul>
                     {initialState.map(test => (
                         <li>
+                            <label><input type='checkbox' /></label>
                             {test.title}
+                            <button>Delete</button>
+                            <button>Edit</button>
                         </li>
                     ))}
                 </ul>
             </div>
-            
-        </>
     )
 }
