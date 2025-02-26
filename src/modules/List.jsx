7080bf8c8668c1ac2183baecf7c53b94725a343a
hairforce1x <93629403+hairforce1x@users.noreply.github.com>
2025-02-25 22:07:32 -0700

    Got onClick to work but broke everything else

--- src/modules/List.jsx
+++ src/modules/List.jsx
@@ -1,14 +1,16 @@
 import initialState from '../data'
 import { useState } from 'react'
-
+let clickCount = 0;
 export default function List() {
     const [task, setTask] = useState('');
+    {console.log('test:' + task)}
     return (
             <div>
                 <input value={task} onChange={e => setTask(e.target.value)} />
-                console.log(task)
                 <button onClick={() => {
-                    setTask('')
+                    clickCount++;
+                    let task = setTask(clickCount)
+                    console.log('clicked: ' + task)
                 }}>add task</button>
                 <ul>
                     {initialState.map(test => (
